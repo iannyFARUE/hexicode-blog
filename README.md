eaab47e6101610e9f57f950fc733f395ab1ca56b
Ian Madhara <iannyfarai@gmail.com>
2024-10-25 01:55:37 -0400

    added start up instructions

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # hexicode-blog
 
 A fast api blog to share my blog post with easy
+
+# starting app
+
+```
+fastapi dev main.py
+```
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -9,8 +10,35 @@ def index():
         'data':{'name':'Ian'}
     }
 
-@app.get('/about')
-def about():
+@app.get('/blog')
+def index(limit=40,published: bool = True, sort: Optional[str]=None):
+    if published:
+        return {
+            'data':f'50 from database'
+        }
+    else:
+        return {'data':f'{limit} from DB in else'}
+
+
+@app.get('/blog/unpublished')
+def unpublished():
     return {
-        'data':'This is new'
+        'data': 'Unplished type'
     }
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {
+        'data':id
+    }
+
+@app.get("/blog/{id}/comments")
+def comment(id:int):
+    # fetch comments of blog by id = id
+    return {'id':{'id':id}}
+
+
+@app.post('/blog')
+def create_blog():
+    return {'data':'Post created'}
